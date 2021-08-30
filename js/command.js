d0732d3818a9118dee508e9911704a2187ee703c
Eren Kahraman <kahraman1234eren@gmail.com>
2021-08-30 18:33:18 +0300

    .

--- js/command.js
+++ js/command.js
@@ -1,21 +1,24 @@
 const { prefix } = require("../json/config.json"); //calling prefix variable from json file
 
-module.exports = (client, aliases, callback) => {    //command handler 
+module.exports = (client, aliases, callback) => {
+  //command handler
 
-    if (typeof aliases === "string"){   // if aliases is a string puts it in an array
-        aliases = [aliases]
-    }
+  if (typeof aliases === "string") {
+    // if aliases is a string puts it in an array
+    aliases = [aliases];
+  }
 
-    client.on("message", message => { //listens messages and responds them
-        const { content } = message
+  client.on("message", (message) => {
+    //listens messages and responds them
+    const { content } = message;
 
-        aliases.forEach((alias) => {
-            const command = "${prefix}${alias}"
+    aliases.forEach((alias) => {
+      const command = "${prefix}${alias}";
 
-            if (content.startsWith("${command} ") || content == command) {
-                console.log("Running command ${command}!")
-                callback(message)
-            }
-        })
-    })
-}
+      if (content.startsWith("${command} ") || content == command) {
+        console.log("Running command ${command}!");
+        callback(message);
+      }
+    });
+  });
+};
--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
-const { Client, Intents } = require('discord.js');
-const { token } = require('../json/config.json');
+const { Client, Intents } = require("discord.js");
+const { token } = require("../json/config.json");
 const command = require("./command");
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-	console.log("I'm Ready!");
+client.once("ready", () => {
+  console.log("I'm Ready!");
+  client.user.setActivity("Porno", { type: "WATCHING" });
 
-    command(client, "test", (message) =>{
-
-        message.reply("Test")
-    })
-
-
-
-
-
-
-
+  command(client, "test", (message) => {
+    message.channel.send("Test");
+  });
 });
 
 client.login(token);
