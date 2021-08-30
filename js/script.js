ae8a8ccb637c2d3a2dbb2a227ab62248c0927d01
Eren Kahraman <kahraman1234eren@gmail.com>
2021-08-30 17:28:04 +0300

    initial file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('../config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log("I'm Ready!");
+});
+
+// Login to Discord with your client's token
+client.login(token);
