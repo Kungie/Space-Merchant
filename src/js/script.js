d94df0ed9c4edd76a5c72eccd7ee30b09eb74206
Eren Kahraman <kahraman1234eren@gmail.com>
2021-08-31 22:04:42 +0300

    önemli

--- src/js/script.js
+++ src/js/script.js
@@ -13,39 +13,3 @@ client.once('ready', () => {
 // Login to Discord with your client's token
 client.login(token);
 
-// fs.readdirSync("./src/commands")
-//       .filter(file => file.endsWith(".js"))
-//       .forEach(file => {
-//           const command = require(`./commands/${file}`);
-//           console.log(chalk.blue('Command ${command.name} loaded'))        
-//           client.commands.set(command.name, command);
-//       });
-
-
-// client.on("ready", () => {
-//     console.clear(); 
-
-//     console.log("Bot is online!")
-// });
-
-// client.on("messageCreate", message => {
-	  
-//   console.log(chalk.red(message.content));
-  
-//   if (!message.content.startsWith(config.prefix)) return;
-	
-//   const args = message.content.substring(config.prefix.length).split(/ +/);
-
-//   switch (args[0]) {
-//     case "hello":
-
-//       message.channel.send("Hello")
-//       break;
-
-//     case "say":
-//       message.channel.send(args.slice(1).join(" "));
-//       break;
-//   }
-// });
-
-// client.login(config.token);
