20c8908deac999c68d754d6cbbbc60a7d7c5e727
Eren Kahraman <kahraman1234eren@gmail.com>
2021-08-31 22:07:06 +0300

    .

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,22 @@
-// Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-// Login to Discord with your client's token
-client.login(token);
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+});
+
+client.login(token);
