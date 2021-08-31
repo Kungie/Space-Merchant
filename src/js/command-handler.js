727dff7500c0b9f50cf569f4655e9c375b253a63
Eren Kahraman <kahraman1234eren@gmail.com>
2021-08-31 22:55:14 +0300

    Perfecto

--- src/js/command-handler.js
+++ src/js/command-handler.js
@@ -1,13 +1,13 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientID, guildID, token } = require('./json/config.json');
+const { clientID, guildID, token } = require('../json/config.json');
 const fs = require('fs');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('../commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`../commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
