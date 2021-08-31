930a0dcd084ada3bf09607a29cb4c6ec112cd312
Eren Kahraman <kahraman1234eren@gmail.com>
2021-08-31 21:57:40 +0300

    .

--- src/js/script.js
+++ src/js/script.js
@@ -5,39 +5,39 @@ const config = require("../json/config.json");
 const Client = require("../base/client");
 const client = new Client();
 
-fs.readdirSync("./src/commands")
-      .filter(file => file.endsWith(".js"))
-      .forEach(file => {
-          const command = require(`./commands/${file}`);
-          console.log(chalk.blue('Command ${command.name} loaded'))        
-          client.commands.set(command.name, command);
-      });
+// fs.readdirSync("./src/commands")
+//       .filter(file => file.endsWith(".js"))
+//       .forEach(file => {
+//           const command = require(`./commands/${file}`);
+//           console.log(chalk.blue('Command ${command.name} loaded'))        
+//           client.commands.set(command.name, command);
+//       });
 
 
-client.on("ready", () => {
-    console.clear(); 
+// client.on("ready", () => {
+//     console.clear(); 
 
-    console.log("Bot is online!")
-});
+//     console.log("Bot is online!")
+// });
 
-client.on("messageCreate", message => {
+// client.on("messageCreate", message => {
 	  
-  console.log(chalk.red(message.content));
+//   console.log(chalk.red(message.content));
   
-  if (!message.content.startsWith(config.prefix)) return;
+//   if (!message.content.startsWith(config.prefix)) return;
 	
-  const args = message.content.substring(config.prefix.length).split(/ +/);
+//   const args = message.content.substring(config.prefix.length).split(/ +/);
 
-  switch (args[0]) {
-    case "hello":
+//   switch (args[0]) {
+//     case "hello":
 
-      message.channel.send("Hello")
-      break;
+//       message.channel.send("Hello")
+//       break;
 
-    case "say":
-      message.channel.send(args.slice(1).join(" "));
-      break;
-  }
-});
+//     case "say":
+//       message.channel.send(args.slice(1).join(" "));
+//       break;
+//   }
+// });
 
-client.login(config.token);
+// client.login(config.token);
