cb9dca6ddea9744d7c9331d9cbc74682ed346db7
Eren Kahraman <kahraman1234eren@gmail.com>
2021-09-01 21:57:18 +0300

    kick permission check

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -9,8 +9,12 @@ module.exports = {
 			.setDescription('The input to target user')
 			.setRequired(true)),
 	async execute(interaction) {
-        await interaction.reply('You have thrown someone into space void!');
-        const member = interaction.options.getMember('target');
-        member.kick();
+		if (interaction.member.permissions.has("KICK_MEMBERS")){
+			await interaction.reply('You have thrown someone into space void!');
+        	const member = interaction.options.getMember('target');
+        	member.kick();
+		} else {
+			interaction.reply("You don't have permission to kick someone.")
+		}
 	},
 };
