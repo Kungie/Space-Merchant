e87556364a4a6c0ddf3894f9a10aa63ca0cda69e
Eren Kahraman <kahraman1234eren@gmail.com>
2021-09-01 21:41:29 +0300

    Ban Command
    added Ban.js

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,10 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { User } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ban')
-		.setDescription('Bans a user from the server'),
+    .setName('kick')
+	.setDescription('Kicks a user!')
+	.addUserOption(option =>
+		option.setName('target')
+			.setDescription('The input to target user')
+			.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('You are banned!');
+        await interaction.reply('You have thrown someone into space void!');
+        const member = interaction.options.getMember('target');
+        member.kick();
 	},
 };
